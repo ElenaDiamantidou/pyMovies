7eadaf3ec6ff13ca26e43559b4336f5befbccf6c
Eleni Diamantidou <elenadiamantidou@gmail.com>
2018-02-24 16:52:28 +0200

    change tranfer learning

--- Tensorflow/buildDataset.py
+++
@@ -1,9 +0,0 @@
-from dataset import load_cached
-import sys
-
-directory = sys.argv[1]
-dataset = load_cached(cache_path='my_dataset.pkl', in_dir=directory)
-num_classes = dataset.num_classes
-class_names = dataset.class_names
-print num_classes
-print class_names
